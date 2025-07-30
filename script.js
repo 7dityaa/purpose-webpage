fd54f8c99cc1e1ac37d8590fbb098edfd2256435
7dityaa <adityaadhikari155@gmail.com>
2025-07-30 13:49:01 +0530

    Initial commit: My webpage files

---
+++ script.js
@@ -0,0 +1,19 @@
+const noBtn = document.getElementById('noBtn');
+const yesBtn = document.getElementById('yesBtn');
+let yesButtonScale = 1;
+
+noBtn.addEventListener('mouseover', () => {
+    const x = Math.floor(Math.random() * (window.innerWidth - 150));
+    const y = Math.floor(Math.random() * (window.innerHeight - 150));
+    noBtn.style.position = 'absolute';
+    noBtn.style.left = `${x}px`;
+    noBtn.style.top = `${y}px`;
+
+    
+    yesButtonScale += 1;
+    yesBtn.style.transform = `scale(${yesButtonScale})`;
+});
+
+yesBtn.addEventListener('click', () => {
+    window.location.href = 'page2/page.html';
+});
